5ac1740b93f9b149d59fcd01bf29cf14ab30898e
SumanaMeur16 <marketer.sumana@gmail.com>
2025-09-06 00:46:05 +0530

    call history function added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
 
-function message(){
+function message(Data){
     let totalCoins = document.getElementById("coins")
     let nowCoins = totalCoins.innerText - 20;
-    totalCoins.textContent = nowCoins
     
-    if(coins < 20){
+
+    if(totalCoins.innerText < 20){
         alert("Insufficient Coin Balance")
     }
-
-
-    // alert ("button clicked");
+    else{
+       let serviceName = document.getElementById(Data).querySelector("h2").innerText;
+       let serviceNumber = document.getElementById(Data).querySelector("h3").innerText;
+       alert(`Calling ${serviceName} at ${serviceNumber}`)
+        totalCoins.textContent = nowCoins
+    }
 }
+
+function makeACall(Data){
+    message(Data)
+    let serviceName = document.getElementById(Data).querySelector("h2").innerText;
+    let serviceNumber = document.getElementById(Data).querySelector("h3").innerText;
+    const now = new Date();
+    const myTime = now.toLocaleTimeString();
+
+    let newCallHistory = document.getElementById("callHistory")
+    newCallHistory.innerHTML += `<div class="bg-slate-100 rounded-lg w-full flex items-center px-4 py-3 my-2">
+                        <div class="w-2/3 text-start">
+                            <p class="font-bold">${serviceName}</p>
+                            <p>${serviceNumber}</p>
+                        </div>
+                        <div class="w-1/3 text-center">
+                           ${myTime}
+                        </div>
+                    </div>`   
+   
+    
+}
+
